7b56242ac05c2824e003bd04716e679beac358a5
Kelvin <kelvin.chan.hkg@gmail.com>
2020-12-28 23:21:44 +0800

    finalize index page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,3 +3,13 @@ console.log('Hello!');
 $(document).ready(() => {
   console.log('HesSchool Hello!');
 });
+
+// window.onload = function () {
+//   if (window.jQuery) {
+//     // jQuery is loaded  
+//     alert("Yeah!");
+//   } else {
+//     // jQuery is not loaded
+//     alert("Doesn't Work");
+//   }
+// }
