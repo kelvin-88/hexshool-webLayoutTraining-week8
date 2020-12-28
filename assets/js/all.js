c0b1357a3f6078cd024b7c287cf014e755a768c2
Kelvin <kelvin.chan.hkg@gmail.com>
2020-12-28 23:22:42 +0800

    Update 2020-12-28T15:22:19.721Z

---
+++ assets/js/all.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+}); // window.onload = function () {
+//   if (window.jQuery) {
+//     // jQuery is loaded  
+//     alert("Yeah!");
+//   } else {
+//     // jQuery is not loaded
+//     alert("Doesn't Work");
+//   }
+// }
+//# sourceMappingURL=all.js.map
